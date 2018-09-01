f3de946e3175b79e52059e807fcdc2ec8263d243
jkurthoconnor <jkurthoconnor@gmail.com>
2018-09-01 13:55:17 -0600

    add api object; all api requests work

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -4,4 +4,96 @@ $(function() {
 
   $('body').append(mainScript({}));
 
+  const api = {  // id args can be passed as num or str
+    getList: function() {
+      $.ajax({
+        url: `http://localhost:4567/api/todos`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+          console.log(json);
+        },
+      });
+    },
+
+    getTodo: function(id) {
+      $.ajax({
+        url: `http://localhost:4567/api/todos/${id}`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+          console.log(json);
+        }
+      });
+    },
+
+    saveNewTodo: function(jsonObj) {
+      $.ajax({
+        url: `http://localhost:4567/api/todos`,
+        type: 'POST',
+        data: jsonObj,
+        dataType: 'json',
+        success: function(json) {
+          console.log(json);
+        },
+      });
+    },
+
+    updateTodo: function(id, jsonObj) {
+      $.ajax({
+        url: `http://localhost:4567/api/todos/${id}`,
+        type: 'PUT',
+        data: jsonObj,
+        dataType: 'json',
+        success: function(json) {
+          console.log(json);
+        },
+      });
+    },
+
+    deleteTodo: function(id) {
+      $.ajax({
+        url: `http://localhost:4567/api/todos/${id}`,
+        type: 'DELETE',
+        statusCode: {
+          204: function() {
+          console.log('deleted');
+          }
+        },
+      });
+    },
+
+    toggleTodoCompletion: function(id) {
+      $.ajax({
+        url: `http://localhost:4567/api/todos/${id}/toggle_completed`,
+        type: 'POST',
+        success: function(json) {
+          console.log(json);
+        },
+      });
+    },
+  };
+
+
+
+  let sampleTodo = {
+    "title": "Hello",
+    "month": "01",
+    "day": "11",
+    "year": "2017",
+    "description": "do stuff all day",
+    "completed": "false",
+  };
+
+
+  let sampleUpdate = {
+    "title": "Hello",
+    "month": "01",
+    "day": "11",
+    "year": "2017",
+    "description": "do stuff in morning",
+    "completed": "false",
+  };
+
 });
+
